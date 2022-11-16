292d0f6ae5433912ad9bcadc444e8495bb542d81
Samuel Perales <samuelperales4@gmail.com>
2022-11-16 14:47:24 -0600

    center tile and keyboard control

--- reader.py
+++ reader.py
@@ -1,82 +1,67 @@
+import keyboard
 import pygame
 import usb.core
 import usb.util
-import numpy as np
 
 VID = 0x0079 #0x18F8 mouse # 0079 dance pad
 PID = 0x0011 #0x0F99 mouse # 0011 dance pad
 
-dev = usb.core.find(idVendor = VID, idProduct = PID)
-if dev is None:
+device = usb.core.find(idVendor = VID, idProduct = PID)
+if device is None:
     raise ValueError('Device not found')
 
-#print(dev)
+# set the active configuration. With no arguments, the first
+# configuration will be the active one
+device.set_configuration()
 
-ep = dev[0].interfaces()[0].endpoints()[0]
-i = dev[0].interfaces()[0].bInterfaceNumber
+# get an endpoint instance
+config = device.get_active_configuration()
+interface = config[(0,0)]
 
-#dev.reset()
+endpoint = usb.util.find_descriptor(
+    interface,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_IN)
 
-# This doesn't work on Windows
-#if dev.is_kernel_driver_active(i):
-#    dev.detach_kernel_driver(i)
+assert endpoint is not None
 
-dev.set_configuration()
-eaddr = ep.bEndpointAddress
-#print(eaddr)
+endpoint_address = endpoint.bEndpointAddress
+packet_size = endpoint.wMaxPacketSize
 
-'''for i in range(0):
-    r = dev.read(eaddr, 8)
-    center = format(r[4], '08b')
-    arrows = format(r[5], '08b')
-    other = format(r[6], '08b')
-    print(center, arrows, other)'''
+# used for placing pygame window on top of all other windows
+import win32gui
+import win32con
 
-def button_released(previous, current):
-    released = []
-    for i in range(len(previous)):
-        if previous[i] != current[i]:
-            released.append(i)
-    return released
-
-class Memory():
-    def __init__(self, n = 51):
-        self.n = n
-        self.grid = np.zeros((n, n))
-
-class Interpreter():
-    def __init__(self):
-        self.memory = Memory()
-        self.pointer = (int(self.memory.n/2), int(self.memory.n/2))
-
-    def run(self, program):
-        commands = [c for c in program]
-        dict = {'←': (-1, 0), '↑': (0, -1), '→': (1, 0), '↓': (0, 1),
-                '↖': (-1, -1), '↗': (1, -1), '↘': (1, 1), '↙': (-1, 1)}
-        for command in commands:
-            pass
-
-print('←↑→↓↖↗↘↙')
+def on_top(window):
+    win32gui.SetWindowPos(window, win32con.HWND_TOPMOST,100,100,200,200,0)
 
 if __name__ == '__main__':
     win = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('Dance Dance Revolution')
     pygame.font.init()
 
-    u = pygame.transform.scale(pygame.image.load('other/ddr/arrows/u.png'), (150, 150))
-    d = pygame.transform.scale(pygame.image.load('other/ddr/arrows/d.png'), (150, 150))
-    l = pygame.transform.scale(pygame.image.load('other/ddr/arrows/l.png'), (150, 150))
-    r = pygame.transform.scale(pygame.image.load('other/ddr/arrows/r.png'), (150, 150))
+    center = pygame.transform.scale(pygame.image.load('arrows/center.png'), (150, 150))
+
+    up = pygame.transform.scale(pygame.image.load('arrows/up.png'), (150, 150))
+    down = pygame.transform.scale(pygame.image.load('arrows/down.png'), (150, 150))
+    left = pygame.transform.scale(pygame.image.load('arrows/left.png'), (150, 150))
+    right = pygame.transform.scale(pygame.image.load('arrows/right.png'), (150, 150))
     
-    ul = pygame.transform.scale(pygame.image.load('other/ddr/arrows/ul.png'), (150, 150))
-    ur = pygame.transform.scale(pygame.image.load('other/ddr/arrows/ur.png'), (150, 150))
-    dl = pygame.transform.scale(pygame.image.load('other/ddr/arrows/dl.png'), (150, 150))
-    dr = pygame.transform.scale(pygame.image.load('other/ddr/arrows/dr.png'), (150, 150))
+    up_left = pygame.transform.scale(pygame.image.load('arrows/up_left.png'), (150, 150))
+    up_right = pygame.transform.scale(pygame.image.load('arrows/up_right.png'), (150, 150))
+    down_left = pygame.transform.scale(pygame.image.load('arrows/down_left.png'), (150, 150))
+    down_right = pygame.transform.scale(pygame.image.load('arrows/down_right.png'), (150, 150))
 
-    program = ''
+    pressed = 0
+    def press(keys, debounce = 30):
+        global pressed
+        if pressed == 0:
+            keyboard.press_and_release(keys)
+            pressed = debounce
 
-    previous_arrows, previous_other = [], []
-    current_arrows, current_other = [], []
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -85,38 +70,45 @@ if __name__ == '__main__':
         
         win.fill((255, 255, 255))
 
-        read = dev.read(eaddr, 8)
-        #print(read)
-        center = format(read[4], '08b')
-        arrows = format(read[5], '08b')
-        other = format(read[6], '08b')
+        on_top(pygame.display.get_wm_info()['window'])
 
-        previous_arrows = current_arrows
-        previous_other = current_other
-        current_arrows = arrows
-        current_other = other
+        data = device.read(endpoint_address, packet_size)
 
-        for index in button_released(previous_arrows, current_arrows):
-            c = ['←','↑','→','↓']
-            program += c[index]
-        print(program)
+        middle = format(data[4], '08b')
+        arrows = format(data[5], '08b')
+        other = format(data[6], '08b')
+
+        if int(middle[0]) == 1:
+            win.blit(center, (170, 170))
+            press('space')
 
         if int(arrows[0]) == 1:
-            win.blit(r, (330, 170))
+            win.blit(right, (330, 170))
+            press('right')
         if int(arrows[1]) == 1:
-            win.blit(u, (170, 10))
+            win.blit(up, (170, 10))
+            press('up')
         if int(arrows[2]) == 1:
-            win.blit(d, (170, 330))
+            win.blit(down, (170, 330))
+            press('down')
         if int(arrows[3]) == 1:
-            win.blit(l, (10, 170))
+            win.blit(left, (10, 170))
+            press('left')
 
         if int(other[4]) == 1:
-            win.blit(ur, (330, 10))
+            win.blit(up_right, (330, 10))
+            press(['up', 'right'])
         if int(other[5]) == 1:
-            win.blit(ul, (10, 10))
+            win.blit(up_left, (10, 10))
+            press(['up', 'left'])
         if int(other[6]) == 1:
-            win.blit(dr, (330, 330))
+            win.blit(down_right, (330, 330))
+            press(['down', 'right'])
         if int(other[7]) == 1:
-            win.blit(dl, (10, 330))
+            win.blit(down_left, (10, 330))
+            press(['down', 'left'])
+
+        if pressed > 0:
+            pressed -= 1
 
         pygame.display.update()
